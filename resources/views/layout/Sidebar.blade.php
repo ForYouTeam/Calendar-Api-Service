e0769e16e7b76f719b010f9a085062368e1de5a7
husnulmaniah <husnulmaniah86@gmail.com>
2022-06-27 18:46:51 +0800

    modified view

--- resources/views/layout/Sidebar.blade.php
+++ resources/views/layout/Sidebar.blade.php
@@ -1,15 +1,20 @@
 <ul class="list-unstyled components">
     <li class="active">
         <a href="#dashboard" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i
-                class="fa fa-dashboard yellow_color"></i> <span>Dashboard</span></a>
+                class="fa fa-dashboard yellow_color"></i> <span>Master Data</span></a>
         <ul class="collapse list-unstyled" id="dashboard">
             <li>
-                <a href="dashboard.html">> <span>Default Dashboard</span></a>
+                <a href="dashboard.html"><i class="fa fa-users blue_color"></i> <span>Data Staff</span></a>
             </li>
             <li>
-                <a href="dashboard_2.html">> <span>Dashboard style 2</span></a>
+                <a href="dashboard_2.html"><i class="fa fa-calendar-o blue_color"></i> <span>Data Detail
+                        Kegiatan</span></a>
+            </li>
+            <li>
+                <a href="dashboard_2.html"><i class="fa fa-gears blue_color"></i> <span>Data Calendar Service</span></a>
             </li>
         </ul>
     </li>
-    <li><a href="widgets.html"><i class="fa fa-clock-o orange_color"></i> <span>Widgets</span></a>
+    <li><a href="widgets.html"><i class="fa fa-calendar green_color"></i> <span>Data Kegiatan</span></a>
+    <li><a href="widgets.html"><i class="fa fa-folder-open-o purple_color"></i> <span>Data Arsip</span></a>
 </ul>
--- resources/views/layout/Topbar.blade.php
+++ resources/views/layout/Topbar.blade.php
@@ -1,7 +1,6 @@
 <div class="topbar">
     <nav class="navbar navbar-expand-lg navbar-light">
         <div class="full">
-            <button type="button" id="sidebarCollapse" class="sidebar_toggle"><i class="fa fa-bars"></i></button>
             <div class="right_topbar">
                 <div class="icon_info">
                     <ul class="user_profile_dd">
