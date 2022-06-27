6e9dbb97cbd59adbdbd3ad1355a2f6636b2c8bee
husnulmaniah <husnulmaniah86@gmail.com>
2022-06-27 19:14:08 +0800

    request migration database

---
+++ app/Http/Requests/CalendarServiceRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class CalendarServiceRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+    public function rules()
+    {
+        return [
+            'calendar_id' => 'required',
+            'sentUpdate' => 'required',
+            'startDate' => 'required',
+            'endDate' => 'required',
+            'name' => 'required',
+            'location' => 'required',
+            'status' => 'required',
+            'description' => 'required',
+            'reminderOvveridesMinutes' => 'required',
+            'reminderUsedefault' => 'required',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'response' => array(
+                'icon' => 'error',
+                'title' => 'Validasi Gagal',
+                'message' => 'Data yang di input tidak tervalidasi',
+            ),
+            'errors' => array(
+                'length' => count($validator->errors()),
+                'data' => $validator->errors()
+            ),
+        ], 422));
+    }
+}
