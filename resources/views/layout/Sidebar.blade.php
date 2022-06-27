74b2562d5cb4fae54764f538bc2b883a92d3059b
IPKIN9 <irwandirini@gmail.com>
2022-06-27 18:16:47 +0800

    Installation template

---
+++ resources/views/layout/Sidebar.blade.php
@@ -0,0 +1,15 @@
+<ul class="list-unstyled components">
+    <li class="active">
+        <a href="#dashboard" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i
+                class="fa fa-dashboard yellow_color"></i> <span>Dashboard</span></a>
+        <ul class="collapse list-unstyled" id="dashboard">
+            <li>
+                <a href="dashboard.html">> <span>Default Dashboard</span></a>
+            </li>
+            <li>
+                <a href="dashboard_2.html">> <span>Dashboard style 2</span></a>
+            </li>
+        </ul>
+    </li>
+    <li><a href="widgets.html"><i class="fa fa-clock-o orange_color"></i> <span>Widgets</span></a>
+</ul>
