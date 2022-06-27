38aab4857837b23d979ea707213247ffe4b84a52
husnulmaniah <husnulmaniah86@gmail.com>
2022-06-27 18:46:19 +0800

    modified migration

---
+++ database/migrations/2022_06_28_082704_create_kegiatan_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKegiatanTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('kegiatan', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('kegiatan_id')->constrained('detail_kegiatan');
+            $table->date('tgl');
+            $table->string('kegiatan');
+            $table->dateTime('waktu');
+            $table->string('keterangan');
+            $table->timestamps();
+        });
+    }
+    public function down()
+    {
+        Schema::dropIfExists('kegiatan');
+    }
+}
